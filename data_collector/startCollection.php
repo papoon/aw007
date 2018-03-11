2f3dbfe8c1b0475ec356a29def50c486c9502531
kezhaya <thanathae@gmail.com>
2018-03-11 23:36:12 +0000

    wip data collector + starting script (php startCollection.php)

---
+++ data_collector/startCollection.php
@@ -0,0 +1,10 @@
+<?php
+  include_once '../data_collector/dataCollector.php';
+
+  $numberDiseases = 2;
+  $numberElements = 5;
+
+  $collector = new DataCollector($numberDiseases, $numberElements);
+  $collector->startCollection();
+
+?>
