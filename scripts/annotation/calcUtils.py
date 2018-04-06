929809f34c2c64f5291d282122d850e79dc9c31b
Unknown <thanathae@gmail.com>
2018-04-06 16:08:55 +0100

    refactoring

--- scripts/annotation/calcUtils.py
+++ scripts/annotation/calcUtils.py
@@ -73,43 +73,11 @@ def calculateTFIDF():
 
     #calculate TF for article terms
     #articleTfValues: key is term, value is list of tuples (article['id'], tf_value)
-    articleTfValues = {}
-    #iterate by all articles
-    for key, value in articleTermsDict.items():
-        #key is tuple (article['did'], article['id'])
-        #value is tuple (DOID, term)
-        term_list = [tup[1] for tup in value]
-        uniqueTermsInDoc = set(term_list)
-        for term in uniqueTermsInDoc:
-            #add to unique terms set
-            uniqueTerms.add(term)
-            #calculate TF value
-            tf_value = calculateTF(term, value)
-            #save TF value for this document
-            if term in articleTfValues:
-                articleTfValues[term] += [(key[1], tf_value)]
-            else:
-                articleTfValues[term] = [(key[1], tf_value)]
+    articleTfValues = auxTFCalculation(articleTermsDict, uniqueTerms)
 
     #calculate TF for tweet terms
     #articleTfValues: key is term, value is list of tuples (tweet['id'], tf_value)
-    tweetTfValues = {}
-    #iterate by all tweets
-    for key, value in tweetTermsDict.items():
-        #key is tuple (tweet['did'], tweet['id'])
-        #value is tuple (DOID, term)
-        term_list = [tup[1] for tup in value]
-        uniqueTermsInDoc = set(term_list)
-        for term in uniqueTermsInDoc:
-            #add to unique terms set
-            uniqueTerms.add(term)
-            #calculate TF value
-            tf_value = calculateTF(term, value)
-            #save TF value for this document
-            if term in tweetTfValues:
-                tweetTfValues[term] += [(key[1], tf_value)]
-            else:
-                tweetTfValues[term] = [(key[1], tf_value)]
+    tweetTfValues = auxTFCalculation(tweetTermsDict, uniqueTerms)
 
     #calculate IDF for all found unique terms
     idfValues = {}
@@ -128,18 +96,55 @@ def calculateTFIDF():
     # print(idfValues)
 
     #calculate TF-IDF for Articles
-    for key, value in articleTfValues.items():
-        #get idf value for term
-        idf_value = idfValues[key]
-        #key is term, value is list of tuples (article['id'], tf_value)
-        for element in value:
-            #TF-IDF = TF * IDF (rounded to 4 decimal cases)
-            tf_idf_value = round(element[1] * idf_value, 4)
-            #save TF-IDF in Tf_Idf_Articles table
-            saveTfIdfInformation(Table_Tf_Idf_Articles, key, element[0], tf_idf_value)
+    auxTFIDFCalculation(articleTfValues, idfValues, Table_Tf_Idf_Articles)
 
     #calculate TF-IDF for Tweets
-    for key, value in tweetTfValues.items():
+    auxTFIDFCalculation(tweetTfValues, idfValues, Table_Tf_Idf_Tweets)
+
+    #pass already calculated values to next method
+    return dictsList
+
+
+def auxTFCalculation(docDictTerms, uniqueTerms):
+    """
+    Calculates TF-IDF for all terms in all documents (articles or tweets).
+    Requires: docDictTerms, one of the dicts result of entityAnnotation method;
+              uniqueTerms, set where unique terms for IDF are stored.
+    Ensures: TF-IDF calculation for all terms in all given documents and return
+    of a dictionary with said values.
+    """
+    #docTfValues: key is term, value is list of tuples (doc['id'], tf_value)
+    docTfValues = {}
+    #iterate by all docs
+    for key, value in docDictTerms.items():
+        #key is tuple (doc['did'], doc['id'])
+        #value is tuple (DOID, term)
+        term_list = [tup[1] for tup in value]
+        uniqueTermsInDoc = set(term_list)
+        for term in uniqueTermsInDoc:
+            #add to unique terms set
+            uniqueTerms.add(term)
+            #calculate TF value
+            tf_value = calculateTF(term, value)
+            #save TF value for this document
+            if term in docTfValues:
+                docTfValues[term] += [(key[1], tf_value)]
+            else:
+                docTfValues[term] = [(key[1], tf_value)]
+    #return result
+    return docTfValues
+
+def auxTFIDFCalculation(tfDict, idfValues, tableToSave):
+    """
+    Auxiliary function to calculate similarity between all diseases and all terms
+    in all documents in a dictionary (articles or tweets).
+    Requires: tfDict, result of auxTFCalculation method for articles or tweets;
+              idfValues, dict of IDF values for all terms;
+              tableToSave, variable Table_Tf_Idf_Articles or Table_Tf_Idf_Tweets.
+    Ensures: TF-IDF calculation for all terms in all given documents and database
+    insertion of the values for posterior use in relevance calculations.
+    """
+    for key, value in tfDict.items():
         #get idf value for term
         idf_value = idfValues[key]
         #key is term, value is list of tuples (tweet['id'], tf_value)
@@ -147,18 +152,15 @@ def calculateTFIDF():
             #TF-IDF = TF * IDF (rounded to 4 decimal cases)
             tf_idf_value = round(element[1] * idf_value, 4)
             #save TF-IDF in Tf_Idf_Tweets table
-            saveTfIdfInformation(Table_Tf_Idf_Tweets, key, element[0], tf_idf_value)
-
-    #pass already calculated values to next method
-    return dictsList
+            saveTfIdfInformation(tableToSave, key, element[0], tf_idf_value)
 
 def calculateSimilarity(dictsList):
     """
     Calculates similarity between all diseases and all terms in all documents
     (articles and tweets).
     Requires: no args.
-    Ensures: TF-IDF calculation for all terms in all documents and database
-    insertion of the values for posterior use in relevance calculations.
+    Ensures: Semantic similarity calculation for all terms in all documents and
+    database insertion of the values for posterior use in relevance calculations.
     """
     #just some pointers for name clarification
     articleTermsDict = dictsList[0]
@@ -184,8 +186,8 @@ def auxSimilarity(diseaseInfo, termsDict, tableToSave):
     Requires: diseaseInfo, result of getAllDiseaseInformation method;
               termsDict, dict of article terms or tweet terms;
               tableToSave, variable Table_Sim_Articles or Table_Sim_Tweets.
-    Ensures: TF-IDF calculation for all terms in all given documents and database
-    insertion of the values for posterior use in relevance calculations.
+    Ensures: Semantic similarity calculation for all terms in all given documents
+    and database insertion of the values for posterior use in relevance calculations.
     """
     #iterate by all diseases
     for disease in diseaseInfo:
