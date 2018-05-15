b38e1966e6a476fcb89f6639a157589394bbc480
Unknown <thanathae@gmail.com>
2018-05-15 22:56:39 +0100

    added twig.js to render templates in browser

--- public_html/assets/js/util.js
+++ public_html/assets/js/util.js
@@ -1,28 +1,20 @@
-function cleanTemplate(html) {
-  return html && html.replace(/\{\%.*\%\}/g,'');
-}
+var twigTemplateCache = {};
 
-function getModelValueForPath(model, path) {
-  path = path.trim();
-  var parts = path.split('.');
-  var value = model;
-  for (var i = 0; i < parts.length; i++) {
-    var part = parts[i];
-    value = value[part];
-    if (!value) {
-      // value not found so we return the path
-      console.error('Path [' + path + '] not found in', model);
-      return path;
-    }
+function loadTwigTemplate(url, callback) {
+  var template = twigTemplateCache[url];
+
+  if (template) {
+    callback(template);
+  } else {
+    $.get(url, function(templateData) {
+      var template = Twig.twig({
+        id: url,
+        data: templateData
+      });
+
+      twigTemplateCache[url] = template;
+
+      callback(template);
+    });
   }
-
-  return value;
-}
-
-function applyTemplate(template, model) {
-  var replacer = function(match, fieldPath) {
-    return getModelValueForPath(model, fieldPath);
-  };
-
-  return template.replace(/\{\{([^\}]+)\}\}/g, replacer);
 }
